eef1e02df52de0ca1808f5bcd3ae89c5cbbcbd9d
Haris Sabanovic <hasa2303@student.miun.se>
2024-01-19 11:59:00 +0100

    Skapade en js fil som visar den aktuella tiden, uppdaterar varje sekund

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+"use strict"
+
+
+    let time = document.getElementById("current-time");
+
+    function showTime() {
+    let d = new Date();
+    let hour = d.getHours();
+    let minute = d.getMinutes();
+    let seconds = d.getSeconds();
+
+    let realTime = `${hour}:${padZero(minute)}:${padZero(seconds)}`;
+
+    time.innerHTML = realTime;
+    }
+
+
+    function padZero(value) {
+        return value < 10 ? `0${value}` : value;
+    }
+
+    setInterval(showTime, 1000);
+
+    showTime();
+    
+
